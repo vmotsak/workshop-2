0f209d8508f2135665b9eec9569e4783d9c904e3
Vladimir Motsak <vmotsak@gmail.com>
2015-09-23 12:47:51 +0200

    Add Student to Subject Item relation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ActiveRecord::Base
   has_many :participations, dependent: :destroy
   has_many :subject_item_notes, dependent: :destroy
-  has_many :subject_items, dependent: :destroy
+  has_many :subject_items, through: :participations
   accepts_nested_attributes_for :subject_items
 
   validates :first_name, :last_name, presence: true
