f35b273d80f7b5a572d7dba7cffe308574825477
Vladimir Motsak <vmotsak@gmail.com>
2015-09-23 14:29:56 +0200

    Route deprecation fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'students#index'
+
   resources :students do
     get :subjects
   end
+
   resources :teachers
   resources :visitors, only: [:index]
-  get 'subjects', to: 'reports'
+  get 'subjects', to: 'reports#subjects',as: 'report_subjects'
 end
