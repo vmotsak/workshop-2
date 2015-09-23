6e75f1bf119a44b3c9251d647994927adb844028
Vladimir Motsak <vmotsak@gmail.com>
2015-09-23 16:07:47 +0200

    Allow subject item ids as student param

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -27,6 +27,6 @@ class StudentsController < ApplicationController
   private
 
   def student_params
-    params.require(:student).permit(:first_name, :last_name)
+    params.require(:student).permit(:first_name, :last_name, subject_item_ids: [])
   end
 end
