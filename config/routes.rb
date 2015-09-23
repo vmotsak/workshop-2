4efc6b9aeee49e7cbf3e84b7a494dd9983194371
Vladimir Motsak <vmotsak@gmail.com>
2015-09-23 16:17:37 +0200

    Fix teachers subject items

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'visitors#index'
-
   resources :students do
     get :subjects
   end
-
-  resources :teachers
+  resources :teachers do
+    get :subjects
+  end
   resources :visitors, only: [:index]
   get 'subjects', to: 'reports#subjects',as: 'report_subjects'
 end
