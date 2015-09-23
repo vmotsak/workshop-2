f4ee9fe84e290c80a23733f32c52e317ced09538
Vladimir Motsak <vmotsak@gmail.com>
2015-09-23 13:04:58 +0200

    Add student_params

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -22,4 +22,10 @@ class StudentsController < ApplicationController
     student.destroy
     redirect_to students_path, notice: I18n.t('shared.deleted', resource: 'Student')
   end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
 end
